e4c0fae275bad2e071f5ff4389fe9eb93e9d5419
wyyyy <xfxbnb@163.com>
2018-01-31 10:26:19 +0800

    del useless file

--- src/store/users-index.js
+++
@@ -1,8 +0,0 @@
-import ResModule from './../Store/modules/ResModules';
-import UsersModule from "./../Store/modules/UsersModule";
-const vuex_config = new Vuex.Store({
-    modules: {
-        res:ResModule,
-        users:UsersModule
-    }
-});
